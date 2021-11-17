0dc01d2a3bce750d55446b18c7fa824c8c201a16
thinkverse <hallberg.kim@gmail.com>
2021-11-17 09:40:59 +0100

    Solve display soft deleted projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -40,7 +40,9 @@ class ProjectController extends Controller
         Project::destroy($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects = Project::all();
+        $projects = Project::query()
+            ->withTrashed()
+            ->get();
 
         return view('projects.index', compact('projects'));
     }
