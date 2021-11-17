f304e73621c2cf09fd1fe0064065ff58cadf68f2
thinkverse <hallberg.kim@gmail.com>
2021-11-17 09:38:18 +0100

    Solve user mass delete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -75,7 +75,9 @@ class UserController extends Controller
         // SQL: delete from users where id in ($request->users)
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
-        // Insert Eloquent statement here
+        User::query()
+            ->whereIn('id', $request->users)
+            ->delete();
 
         return redirect('/')->with('success', 'Users deleted');
     }
