02877d7af864b0a9777820ea288b1a7ed493540c
veerman72 <64311955+veerman72@users.noreply.github.com>
2021-11-18 09:54:12 +0100

    Task four

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,9 +23,10 @@ class UserController extends Controller
 
     public function check_create($name, $email)
     {
-        // TASK: find a user by $name and $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::firstOrCreate(
+            ['name' => $name, 'email' => $email],
+            ['password' => bcrypt('password')]
+        );
 
         return view('users.show', compact('user'));
     }
