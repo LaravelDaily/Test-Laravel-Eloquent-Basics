44070acdea4cf2a17c379236acdbc41577ade22a
innoflash <innocentmazando@gmail.com>
2021-11-18 14:10:53 +0200

    fixed test_check_or_create_user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -33,7 +35,12 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email
+        ], [
+            'password' => Hash::make(Str::random(10))
+        ]);
 
         return view('users.show', compact('user'));
     }
