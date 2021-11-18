aa68a4bf01f40b19f356b0a818ab34e19c966472
Wilton Baltazar <wiltonbaltazar@gmail.com>
2021-11-18 14:40:02 +0200

    Task Completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::whereNotNull('email_verified_at')->orderBy('created_at', 'desc')->limit(3); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')->latest()->limit(3)->get(); // replace this with Eloquent statement
 
         return view('users.index', compact('users'));
     }
