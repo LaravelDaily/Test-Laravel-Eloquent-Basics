dcd51f6c11355a3a4bc3b9829f792399a73c9f71
veerman72 <64311955+veerman72@users.noreply.github.com>
2021-11-18 11:32:19 +0100

    Task eight

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,11 +44,7 @@ class UserController extends Controller
 
     public function destroy(Request $request)
     {
-        // TASK: delete multiple users by their IDs
-        // SQL: delete from users where id in ($request->users)
-        // $request->users is an array of IDs, ex. [1, 2, 3]
-
-        // Insert Eloquent statement here
+        User::destroy($request->users);
 
         return redirect('/')->with('success', 'Users deleted');
     }
