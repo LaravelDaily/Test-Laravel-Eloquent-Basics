add4d968e2ac58b3bd0aa6186369e626db65b4ef
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-11-18 12:46:15 +0100

    Task 11

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -36,8 +36,7 @@ class ProjectController extends Controller
 
     public function store_with_stats(Request $request)
     {
-        // TASK: on creating a new project, create an Observer event to run SQL
-        //   update stats set projects_count = projects_count + 1
+        print("salut");
         $project = new Project();
         $project->name = $request->name;
         $project->save();
