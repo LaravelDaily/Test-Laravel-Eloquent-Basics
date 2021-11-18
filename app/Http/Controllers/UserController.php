e9e3f7ec45a649c37449e71105e7e5f238e459b3
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-11-18 12:21:47 +0100

    Task 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,10 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')
+            ->orderBy('created_at', 'DESC')
+            ->limit(3)
+            ->get();
 
         return view('users.index', compact('users'));
     }
