e3919ab232b11ee010e3b0654cc45ad89c02eed4
LoaiDev <lolyakram11@gmail.com>
2021-11-19 00:15:12 +0200

    Tests Passed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,54 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Str;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // TASK: turn this SQL query into Eloquent
-        // select * from users
-        //   where email_verified_at is not null
-        //   order by created_at desc
-        //   limit 3
-
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')
+            ->orderByDesc('created_at')
+            ->take(3)
+            ->get();
 
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::findOrFail($userId); // TASK: find user by $userId or show "404 not found" page
 
         return view('users.show', compact('user'));
     }
 
     public function check_create($name, $email)
     {
-        // TASK: find a user by $name and $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL;
-
+        $user = User::firstOrCreate(compact('name', 'email'), ['password' => Str::random()]);
         return view('users.show', compact('user'));
     }
 
     public function check_update($name, $email)
     {
-        // TASK: find a user by $name and update it with $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::firstOrNew(compact('name'), ['password' => Str::random()]);
+        $user->fill(compact('email'))->save();
 
         return view('users.show', compact('user'));
     }
 
     public function destroy(Request $request)
     {
-        // TASK: delete multiple users by their IDs
-        // SQL: delete from users where id in ($request->users)
-        // $request->users is an array of IDs, ex. [1, 2, 3]
-
-        // Insert Eloquent statement here
+        User::whereIn('id', $request->users)->delete();
 
         return redirect('/')->with('success', 'Users deleted');
     }
