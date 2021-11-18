82a1e281256a2edf294a5a239800abf72f132d7b
e.testagrossa <e.testagrossa@digitalpa.it>
2021-11-18 15:30:47 +0100

    first try

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function active()
+    {
+        return $this->email_verified_at !== NULL ;
+    }
 }
