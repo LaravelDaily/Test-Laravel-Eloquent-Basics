cf8f7e0b8e8dc52c05b016f5ba7c96781cac360c
Rahul Bhilesha <bhilesharahul@gmail.com>
2021-11-18 17:49:21 +0530

    Solved all test case errors related to eloquent.

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeActive($query)
+    {
+        return $query->whereNotNull('email_verified_at');
+    }
 }
