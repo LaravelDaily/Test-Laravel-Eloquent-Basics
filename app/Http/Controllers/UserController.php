2be0ccf1f82eebc0818d6ad294d8a29162d36662
Mateus Santana <mateusssantana@gmail.com>
2021-11-18 15:45:36 -0300

    fix: test 8

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -67,6 +67,9 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        User::query()
+            ->whereIn('id', $request->users)
+            ->delete();
 
         return redirect('/')->with('success', 'Users deleted');
     }
