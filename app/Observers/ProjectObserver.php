23512139be90610985aeafa0b5f20024860f541b
jakubzivny <info@jakubzivny.sk>
2021-11-18 09:50:12 +0100

    Done

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Project;
+use App\Models\Stat;
+use Illuminate\Support\Facades\DB;
+
+class ProjectObserver
+{
+    /**
+     * Handle the Project "created" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        //   update stats set projects_count = projects_count + 1
+        Stat::first()->update([
+            'projects_count' => DB::raw('projects_count+1')
+        ]);
+    }
+
+    /**
+     * Handle the Project "updated" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function deleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "restored" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function restored(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "force deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function forceDeleted(Project $project)
+    {
+        //
+    }
+}
