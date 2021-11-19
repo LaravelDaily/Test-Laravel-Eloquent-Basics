9604ddb53ea4553c8d0aa38cffcde2973da393f7
Ryan Baham <caballegend@gmail.com>
2021-11-19 02:27:23 -0600

    Test 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Str;
 
 class UserController extends Controller
 {
@@ -43,7 +45,11 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make(Str::random(10)),
+        ]); // updated or created user
 
         return view('users.show', compact('user'));
     }
