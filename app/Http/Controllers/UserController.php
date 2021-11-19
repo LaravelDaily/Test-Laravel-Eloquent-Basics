2b0d229a7713cc47e04ba31b464a2e112a707f05
Wilsen Hernández <wilsenh95@gmail.com>
2021-11-19 12:44:58 -0400

    ✅ Task 7 completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -50,7 +50,13 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::updateOrCreate([
+            'name' => $name,
+        ],
+        [
+            'email' => $email,
+            'password' => Hash::make(Str::random()),
+        ]); // updated or created user
 
         return view('users.show', compact('user'));
     }
