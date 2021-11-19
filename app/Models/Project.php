ef82fde8738b2307b7fc8567117194b9318db491
Hardik Khorasiya <hardikkhorasiya09@gmail.com>
2021-11-19 18:28:18 +0530

    All Test Case Passed (5 Files Updated)

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,4 +9,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::created(function ($user) {
+            Stat::whereId(1)
+                ->increment("projects_count");
+        });
+    }
 }
