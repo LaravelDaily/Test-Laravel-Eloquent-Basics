75d04f635f6bf9998e4273ab4f0786af3ead60ba
Victor Araque <varaquecasaus@gmail.com>
2021-11-20 16:46:24 +0100

    All task completed!

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+Use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
@@ -17,12 +20,17 @@ class UserController extends Controller
 
         $users = User::all(); // replace this with Eloquent statement
 
+        $users = User::whereNotNull('email_verified_at')
+                        ->limit(3)
+                        ->orderByDesc('created_at')
+                        ->get();
+
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::findOrFail($userId); // TASK: find user by $userId or show "404 not found" page
 
         return view('users.show', compact('user'));
     }
@@ -31,7 +39,10 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::firstOrCreate(
+            ['name' => $name, 'email' => $email],
+            ['password' => Hash::make(Str::random())]
+        );
 
         return view('users.show', compact('user'));
     }
@@ -40,7 +51,10 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::updateOrCreate(
+            ['name' => $name],
+            ['email' => $email, 'password' => Hash::make(Str::random())]
+        ); // updated or created user
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +66,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        User::destroy($request->users);
 
         return redirect('/')->with('success', 'Users deleted');
     }
