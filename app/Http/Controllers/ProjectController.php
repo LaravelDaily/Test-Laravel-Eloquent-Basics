debdd1df16dbdae531bfdfb62d8a77545f014dc2
Krasomir <kmioc@inet.hr>
2021-11-20 20:37:24 +0100

    Completed Laravel Eloquent basic test.

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,6 +26,8 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
+        $project = Project::where('name', $request->old_name)
+            ->update(['name' => $request->new_name]);
 
         return redirect('/')->with('success', 'Projects updated');
     }
@@ -35,7 +37,7 @@ class ProjectController extends Controller
         Project::destroy($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects = Project::all();
+        $projects = Project::withTrashed()->get();
 
         return view('projects.index', compact('projects'));
     }
