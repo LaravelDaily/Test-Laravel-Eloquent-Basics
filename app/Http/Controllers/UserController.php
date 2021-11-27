4434f1de93a84cdb3c560585668cd385b947b6f3
aslambpract <42261475+aslambpract@users.noreply.github.com>
2021-11-27 14:27:41 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = firstOrCreate(['name' => $name , 'email' =>$email]);
+        $user = firstOrCreate(['name' => $name ] , [ 'email' =>$email]);
 
         return view('users.show', compact('user'));
     }
