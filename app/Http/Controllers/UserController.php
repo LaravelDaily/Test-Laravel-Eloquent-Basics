e71396042ddc2e253e843a096ff941105d2c8e2d
dornnsergey <67413092+dornnsergey@users.noreply.github.com>
2021-11-27 12:49:09 +0200

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,7 +33,7 @@ class UserController extends Controller
         //   if not found, create a user with $name, $email and random password
         $user = User::firstOrCreate(
         ['name' => $name, 'email' => $email],
-        ['name' => $name, 'email' => $email, 'password' => 'password'];
+        ['name' => $name, 'email' => $email, 'password' => 'password']
     );
 
         return view('users.show', compact('user'));
