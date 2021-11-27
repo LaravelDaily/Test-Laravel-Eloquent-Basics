0edd253b5b62a8b02caeaccc4dce24f76b807f35
Maxie Wright <maxiewright@gmail.com>
2021-11-26 20:13:21 -0400

    complete 11 eloquent basics task

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PhpParser\Builder;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function scopeActive($query)
+    {
+        return $query->where('email_verified_at', '!=', null);
+    }
 }
