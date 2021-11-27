c5359023e27394b9ad85e2ae3d134b513332cb52
dornnsergey <67413092+dornnsergey@users.noreply.github.com>
2021-11-27 13:05:58 +0200

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::whereNotNull('email_verified')->latest()->take(3)->get();
+        $users = User::whereNotNull('email_verified_at')->latest()->take(3)->get();
 
         return view('users.index', compact('users'));
     }
