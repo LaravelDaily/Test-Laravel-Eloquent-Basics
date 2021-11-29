20693162c32f9c04562bb562d1327be72835d5a6
gbemiro <peculiar.isrl17@gmail.com>
2021-11-29 18:51:15 +0100

    resolve user collection error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -59,8 +59,7 @@ class UserController extends Controller
                 'email' => $email,
             ]);
         } else {
-            $user->email = $email;
-            $user->save();
+            $user::first()->update(['email' => $email]);
         }
 
         return view('users.show', compact('user'));
