1ef604f9515f2e2b4f5f057a2cb2a65ff96c2f04
Yaroslav Kovalenko <yaroslav23@gmail.com>
2021-11-29 22:48:45 +0200

    Implemented all Tasks.

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeActive($query)
+    {
+        $query->whereNot('email_verified_at', 'NULL');
+    }
 }
