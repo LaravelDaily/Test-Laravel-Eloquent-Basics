8fe2e3b875f05ea606799a1589b3236777d711a4
dornnsergey <67413092+dornnsergey@users.noreply.github.com>
2021-12-01 13:16:12 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        User::observe(ProjectObserver::class);
+        Project::observe(ProjectObserver::class);
     }
 }
