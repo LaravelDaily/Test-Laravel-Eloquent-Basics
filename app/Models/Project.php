b7795997d226cee52ddc381a0f84f6b0fba88971
Mohammed Jacksi <moh.jacksi@yahoo.com>
2021-12-06 22:43:56 +0300

    challenge done

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,4 +8,18 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::created(function ($user) {
+            Stat::first()->increment('projects_count');
+        });
+    }
 }
