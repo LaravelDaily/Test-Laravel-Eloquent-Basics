9685349b82f107941e35a92912d821cf7549fd65
Alec Garcia <alec@hyrograsper.com>
2021-12-10 04:30:57 +0000

    Complete Eloquent Tests

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Project;
+use App\Models\Stat;
+
+class ProjectObserver
+{
+    /**
+     * Handle the Project "created" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        Stat::increment('projects_count', 1);
+    }
+}
