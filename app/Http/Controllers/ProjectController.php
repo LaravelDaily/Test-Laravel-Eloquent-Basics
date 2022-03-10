3a21efd4f06702112d0e2b51945fe1230d402044
Luca Di Fazio <highliuk@gmail.com>
2022-03-10 20:38:52 +0100

    Task 5 Solved

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,7 +10,6 @@ class ProjectController extends Controller
 {
     public function store(Request $request)
     {
-        // TASK: Currently this statement fails. Fix the underlying issue.
         Project::create([
             'name' => $request->name
         ]);
@@ -50,5 +49,4 @@ class ProjectController extends Controller
 
         return redirect('/')->with('success', 'Project created');
     }
-
 }
