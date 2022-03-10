217acb17fd3b5ac565852c075f9e920a5b42c941
Luca Di Fazio <highliuk@gmail.com>
2022-03-10 20:26:57 +0100

    Task 2 Solved

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,13 +9,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        // TASK: turn this SQL query into Eloquent
-        // select * from users
-        //   where email_verified_at is not null
-        //   order by created_at desc
-        //   limit 3
-
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')
+            ->latest()
+            ->take(3)
+            ->get();
 
         return view('users.index', compact('users'));
     }
@@ -64,5 +61,4 @@ class UserController extends Controller
 
         return view('users.index', compact('users'));
     }
-
 }
