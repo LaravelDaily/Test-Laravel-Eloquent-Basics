18e6230487743f6eedeee44589602a2330fa811e
basiima <mrbasiima@gmail.com>
2022-04-26 09:56:38 +0300

    Task 1 complete

--- app/Models/Morningnews.php
+++ app/Models/Morningnews.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Morningnews extends Model
 {
     use HasFactory;
-    
+
+    /**
+     * The table associated with the model
+     * 
+     * @var string
+     */
     protected $table = 'morning_news';
 
     protected $fillable = ['title', 'news_text'];
