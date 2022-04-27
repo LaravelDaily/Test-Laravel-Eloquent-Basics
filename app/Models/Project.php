6b7ba7d295955c9ded4868c3040dbaa6c17c58ec
basiima <mrbasiima@gmail.com>
2022-04-27 20:39:37 +0300

    Task 5 complete

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,4 +9,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array
+     */
+    protected $fillable = ['name'];
 }
