0265cafdb5b80a3c362a21b6ad9bd5fadf3f4e3d
basiima <mrbasiima@gmail.com>
2022-04-27 20:30:30 +0300

    Task 4 complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,11 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email,
+            'password' => 'hvdvsjk'
+        ]);
 
         return view('users.show', compact('user'));
     }
