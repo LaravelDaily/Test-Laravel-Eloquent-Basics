7501637070a8bf5df2bbf61eb7b668b836fd14cb
viewtifult <tashanduncan1994@gmail.com>
2022-05-03 14:24:25 +0100

    test github actions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
 
     public function show($userId)
     {
-        $user = User::findOrFail($userId);        // TASK: find user by $userId or show "404 not found" page
+        $user = User::findOrFail($userId); // TASK: find user by $userId or show "404 not found" page
 
         return view('users.show', compact('user'));
     }
