278f4f792ccee4365b273ddcea305cef49bce9bc
Md. Abir Chowdhury <40237786+YouKnowMe2@users.noreply.github.com>
2022-05-26 06:54:38 +0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::where('email_verified_at', '!=', 'null')->orderByDesc('created_at')->limit(3)->get(); // replace this with Eloquent statement
 
         return view('users.index', compact('users'));
     }
