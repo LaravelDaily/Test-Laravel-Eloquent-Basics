3c723ba18b9074000ded7db9b7f82670c82efe79
Md. Abir Chowdhury <40237786+YouKnowMe2@users.noreply.github.com>
2022-05-26 11:27:55 +0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,12 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+       $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email,
+        ], [
+            'password' => Hash::make('Password'),
+        ]);
 
         return view('users.show', compact('user'));
     }
