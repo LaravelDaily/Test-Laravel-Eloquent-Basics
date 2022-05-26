e38d6f77e59621057c62f28220eb387bd76d7414
Md. Abir Chowdhury <40237786+YouKnowMe2@users.noreply.github.com>
2022-05-26 12:11:19 +0600

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function scopeActive($q)
+    {
+        $q->whereNotNull('email_verified_at');
+    }
 }
