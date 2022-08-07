0a3ef98d664cc3ae1a9b5a16ff95ecceeee5f3bd
Gardi <ahmadgardi0@gmail.com>
2022-08-07 22:48:46 +0300

    Task 2 Complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,8 +15,7 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
-
+        $users = User::where('email_verified_at', '!=', null)->orderBy('created_at', 'desc')->limit(3)->get();
         return view('users.index', compact('users'));
     }
 
