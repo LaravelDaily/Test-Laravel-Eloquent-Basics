31c5d2ac38e964b740b5ee57c9715039087133b0
AmirHossein Ebadi <awirhosein@yahoo.com>
2022-08-17 04:06:21 +0430

    Eloquent Test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,14 +15,14 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')->latest()->take(3)->get(); // replace this with Eloquent statement
 
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::findOrFail($userId); // TASK: find user by $userId or show "404 not found" page
 
         return view('users.show', compact('user'));
     }
@@ -31,7 +31,11 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email,
+            'password' => rand(100000, 999999)
+        ]);
 
         return view('users.show', compact('user'));
     }
@@ -40,7 +44,16 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        // updated or created user
+        if ($user = User::where('name', $name)->first()) {
+            $user->update(['email' => $email]);
+        } else {
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => rand(100000, 999999)
+            ]);
+        }
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +65,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        User::destroy($request->users);
 
         return redirect('/')->with('success', 'Users deleted');
     }
@@ -64,5 +78,4 @@ class UserController extends Controller
 
         return view('users.index', compact('users'));
     }
-
 }
