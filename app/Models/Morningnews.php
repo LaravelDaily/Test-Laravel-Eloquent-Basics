1f58dac3e542bd05999ddf230ab8600ac9c5f746
Awfa7 <awfa1996@gmail.com>
2022-08-21 01:51:21 +0300

    test sol

--- app/Models/Morningnews.php
+++ app/Models/Morningnews.php
@@ -10,4 +10,11 @@ class Morningnews extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'news_text'];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'morning_news';
 }
