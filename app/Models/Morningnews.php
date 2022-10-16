97a173366d2450835ad485f277458edac1f04a74
Khaled Ahmed <36667919+KhaledTolba@users.noreply.github.com>
2022-10-16 22:08:16 +0200

    Update test

--- app/Models/Morningnews.php
+++ app/Models/Morningnews.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Morningnews extends Model
-{
+{    
     use HasFactory;
 
     protected $fillable = ['title', 'news_text'];
+    protected $table = "morning_news";
 }
