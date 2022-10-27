4c4c04033a28a314b21631a006b379f83ed4c625
IlijaAngelov <ilija.angelov@yahoo.com>
2022-10-28 00:12:27 +0200

    Test Eloquent Basics

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Stat;
+use App\Models\Project;
+
+class ProjectObserver
+{
+
+    /**
+     * Handle the Project "created" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        Stat::find(1)->increment('projects_count');
+    }
+
+    /**
+     * Handle the Project "updated" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function deleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "restored" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function restored(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "force deleted" event.
+     *
+     * @param  \App\Models\Project  $project
+     * @return void
+     */
+    public function forceDeleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the post "saving" event.
+     *
+     * @param  \App\Post  $post
+     * @return void
+     */
+
+}
