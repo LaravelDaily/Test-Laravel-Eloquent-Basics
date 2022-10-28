446195b22dbbfa9104e918ec2aa2ffee494782ad
Timur <timurcool650@gmail.com>
2022-10-28 14:50:50 +0600

    3 task

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,7 @@ class UserController extends Controller
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::findOrFail($userId);
 
         return view('users.show', compact('user'));
     }
