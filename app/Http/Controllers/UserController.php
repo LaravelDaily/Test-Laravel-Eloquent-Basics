4569dd336142ed024ee22493429ba7bc3e34d5ca
mmdmthr <muhhari@gmail.com>
2022-12-21 09:01:26 +0700

    solve eloquent basic test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -15,14 +16,21 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::active()
+                    ->orderByDesc('created_at')
+                    ->limit(3)
+                    ->get();
 
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::find($userId); // TASK: find user by $userId or show "404 not found" page
+
+        if (!$user) {
+            abort(404);
+        }
 
         return view('users.show', compact('user'));
     }
@@ -31,7 +39,15 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::where('email', $email)->first();
+
+        if (!$user) {
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make('password')
+            ]);
+        }
 
         return view('users.show', compact('user'));
     }
@@ -40,7 +56,18 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::where('name', $name)->first(); // updated or created user
+
+        if ($user) {
+            $user->email = $email;
+            $user->save();
+        } else {
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make('password')
+            ]);
+        }
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +79,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        User::destroy($request->users);
 
         return redirect('/')->with('success', 'Users deleted');
     }
