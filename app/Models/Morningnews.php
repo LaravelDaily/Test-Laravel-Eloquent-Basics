5210f4067b66c01e7cdc2f6195d6cf07211317eb
Wiwid Widyanto <wiwid.widyanto7@gmail.com>
2022-12-29 13:15:06 +0700

    Update Morningnews.php

--- app/Models/Morningnews.php
+++ app/Models/Morningnews.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MorningNews extends Model
+class Morningnews extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'news_text'];
+    protected $table = 'morning_news';
 }
