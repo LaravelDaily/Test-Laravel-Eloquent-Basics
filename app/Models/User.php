7d5edb1a5ced5e62ffe85fea463b4e5c42f0cadb
Newton BOCO <nboco@iitech-benin.net>
2023-01-20 23:12:01 +0100

    all tests done

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function active()
+    {
+        return User::whereNotNull('email_verified_at');
+    }
 }
