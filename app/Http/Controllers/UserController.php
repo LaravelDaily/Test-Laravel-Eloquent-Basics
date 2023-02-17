3aa97764150f5fa1b4736759dab7552cc76566ec
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-02-17 17:04:45 +0500

    test 11 passed successfully

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,11 +46,10 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = User::updateOrCreate(['name' => $name], [
-            'name' => $name,
-            'email' => $email,
-            Hash::make(Str::random(8))
-        ]); // updated or created user
+        $user = User::updateOrCreate(
+            ['name' => $name],
+            ['email' => $email, 'password' => Hash::make('newPassword')]
+        ); // updated or created user
 
         return view('users.show', compact('user'));
     }
