a2578c4b5e6e851fb10aeb5053f9629ea39f801e
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-02-17 16:10:53 +0500

    test 8 passed successfully

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Project;
+use App\Models\Stat;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -27,6 +29,8 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Project::created(function () {
+            Stat::increment('projects_count');
+        });
     }
 }
