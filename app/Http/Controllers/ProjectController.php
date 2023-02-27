f1d6ed2901f8cf3ad392df2eaca88928f093774b
Yeraldi29 <yeraldimujicarosales@gmail.com>
2023-02-27 10:07:26 -0400

    Eloquent tasks completed

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,7 +26,10 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
-
+        $project = Project::where('name',$request->old_name);
+        $project->name = $request->new_name;
+        $project->save();
+        
         return redirect('/')->with('success', 'Projects updated');
     }
 
@@ -35,7 +38,7 @@ class ProjectController extends Controller
         Project::destroy($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects = Project::all();
+        $projects = Project::withTrashed()->get();
 
         return view('projects.index', compact('projects'));
     }
