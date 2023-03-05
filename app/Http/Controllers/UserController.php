324d4376516e1d9c685a30d8fba9c6e9d7089eda
Haythem Dridi <haithemdridiweb@gmail.com>
2023-03-05 23:01:47 +0100

    Task 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,12 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email,
+        ], [
+            'password' => rand(100000, 999999),
+        ]);
 
         return view('users.show', compact('user'));
     }
