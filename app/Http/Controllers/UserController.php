ae634eb10b687da92de35fa845acf4f18423bc82
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-03-15 17:03:51 +0100

    Task 2 ✅

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,62 +7,61 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        // TASK: turn this SQL query into Eloquent
-        // select * from users
-        //   where email_verified_at is not null
-        //   order by created_at desc
-        //   limit 3
+	public function index()
+	{
+		// TASK: turn this SQL query into Eloquent
+		// select * from users
+		//   where email_verified_at is not null
+		//   order by created_at desc
+		//   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+		$users = User::whereNotNull('email_verified_at')->orderByDesc('created_at')->limit(3)->get(); // replace this with Eloquent statement
 
-        return view('users.index', compact('users'));
-    }
+		return view('users.index', compact('users'));
+	}
 
-    public function show($userId)
-    {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+	public function show($userId)
+	{
+		$user = NULL; // TASK: find user by $userId or show "404 not found" page
 
-        return view('users.show', compact('user'));
-    }
+		return view('users.show', compact('user'));
+	}
 
-    public function check_create($name, $email)
-    {
-        // TASK: find a user by $name and $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+	public function check_create($name, $email)
+	{
+		// TASK: find a user by $name and $email
+		//   if not found, create a user with $name, $email and random password
+		$user = NULL;
 
-        return view('users.show', compact('user'));
-    }
+		return view('users.show', compact('user'));
+	}
 
-    public function check_update($name, $email)
-    {
-        // TASK: find a user by $name and update it with $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+	public function check_update($name, $email)
+	{
+		// TASK: find a user by $name and update it with $email
+		//   if not found, create a user with $name, $email and random password
+		$user = NULL; // updated or created user
 
-        return view('users.show', compact('user'));
-    }
+		return view('users.show', compact('user'));
+	}
 
-    public function destroy(Request $request)
-    {
-        // TASK: delete multiple users by their IDs
-        // SQL: delete from users where id in ($request->users)
-        // $request->users is an array of IDs, ex. [1, 2, 3]
+	public function destroy(Request $request)
+	{
+		// TASK: delete multiple users by their IDs
+		// SQL: delete from users where id in ($request->users)
+		// $request->users is an array of IDs, ex. [1, 2, 3]
 
-        // Insert Eloquent statement here
+		// Insert Eloquent statement here
 
-        return redirect('/')->with('success', 'Users deleted');
-    }
+		return redirect('/')->with('success', 'Users deleted');
+	}
 
-    public function only_active()
-    {
-        // TASK: That "active()" doesn't exist at the moment.
-        //   Create this scope to filter "where email_verified_at is not null"
-        $users = User::active()->get();
+	public function only_active()
+	{
+		// TASK: That "active()" doesn't exist at the moment.
+		//   Create this scope to filter "where email_verified_at is not null"
+		$users = User::active()->get();
 
-        return view('users.index', compact('users'));
-    }
-
+		return view('users.index', compact('users'));
+	}
 }
