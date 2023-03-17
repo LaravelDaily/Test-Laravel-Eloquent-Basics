d908bade8b7cf02476e8b07b30a3753a35bfbe99
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2023-03-17 11:30:22 +0100

    Task 8-9 ✅

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,47 +8,47 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function store(Request $request)
-    {
-        // TASK: Currently this statement fails. Fix the underlying issue.
-        Project::create([
-            'name' => $request->name
-        ]);
+	public function store(Request $request)
+	{
+		// TASK: Currently this statement fails. Fix the underlying issue.
+		Project::create([
+			'name' => $request->name
+		]);
 
-        return redirect('/')->with('success', 'Project created');
-    }
+		return redirect('/')->with('success', 'Project created');
+	}
 
-    public function mass_update(Request $request)
-    {
-        // TASK: Transform this SQL query into Eloquent
-        // update projects
-        //   set name = $request->new_name
-        //   where name = $request->old_name
+	public function mass_update(Request $request)
+	{
+		// TASK: Transform this SQL query into Eloquent
+		// update projects
+		//   set name = $request->new_name
+		//   where name = $request->old_name
 
-        // Insert Eloquent statement below
+		// Insert Eloquent statement below
+		Project::where('name', $request->old_name)->update(['name' => $request->new_name]);
 
-        return redirect('/')->with('success', 'Projects updated');
-    }
+		return redirect('/')->with('success', 'Projects updated');
+	}
 
-    public function destroy($projectId)
-    {
-        Project::destroy($projectId);
+	public function destroy($projectId)
+	{
+		Project::destroy($projectId);
 
-        // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects = Project::all();
+		// TASK: change this Eloquent statement to include the soft-deletes records
+		$projects = Project::all();
 
-        return view('projects.index', compact('projects'));
-    }
+		return view('projects.index', compact('projects'));
+	}
 
-    public function store_with_stats(Request $request)
-    {
-        // TASK: on creating a new project, create an Observer event to run SQL
-        //   update stats set projects_count = projects_count + 1
-        $project = new Project();
-        $project->name = $request->name;
-        $project->save();
+	public function store_with_stats(Request $request)
+	{
+		// TASK: on creating a new project, create an Observer event to run SQL
+		//   update stats set projects_count = projects_count + 1
+		$project = new Project();
+		$project->name = $request->name;
+		$project->save();
 
-        return redirect('/')->with('success', 'Project created');
-    }
-
+		return redirect('/')->with('success', 'Project created');
+	}
 }
