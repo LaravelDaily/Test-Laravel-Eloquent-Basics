248d5f9a360daedb0179c57897fea960f1c5b658
kotelnikov.a_qschool.newgrade <ko7elnikoval@yandex.ru>
2023-03-30 15:50:18 +0300

    second try

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,9 +26,8 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
-        Project::where('name', '=',$request->old_name )
-            //->where('name', '=',$request->old_name )
-            ->update('name', '=',$request->new_name );
+        Project::where('name', '=', $request->old_name)
+            ->update(['name', '=', $request->new_name]);
         return redirect('/')->with('success', 'Projects updated');
     }
 
@@ -37,7 +36,7 @@ class ProjectController extends Controller
         Project::destroy($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects =Project::withTrashed()->orderBy('created_at', 'desc')->get();
+        $projects = Project::withTrashed()->orderBy('created_at', 'desc')->get();
 
         return view('projects.index', compact('projects'));
     }
