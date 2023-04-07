e0b8a5a9e8c08261e155af144fd1c0dec3a979de
Lialiuk Yevgen <lyalyuk.ev@gmail.com>
2023-04-07 18:16:13 +0300

    'Eloquent'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,8 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+        //$users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')->orderBy('created_at', 'desc')->limit(3)->get();
 
         return view('users.index', compact('users'));
     }
@@ -23,7 +24,10 @@ class UserController extends Controller
     public function show($userId)
     {
         $user = NULL; // TASK: find user by $userId or show "404 not found" page
-
+        $user = User::find($userId);
+        if (!$user) {
+            abort(404);
+        }
         return view('users.show', compact('user'));
     }
 
@@ -33,6 +37,11 @@ class UserController extends Controller
         //   if not found, create a user with $name, $email and random password
         $user = NULL;
 
+        $user = User::firstOrCreate(
+            ['name' => $name, 'email' => $email],
+            ['name' => $name, 'email' => $email, 'password' => 'password']
+        );
+
         return view('users.show', compact('user'));
     }
 
@@ -40,7 +49,9 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::updateOrCreate(
+            ['name' => $name, 'email' => $email],
+            ['name' => $name, 'email' => $email, 'password' => 'password']);
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +63,8 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+
+        User::destroy($request->users);
 
         return redirect('/')->with('success', 'Users deleted');
     }
