faa28a9caa2c03799531d6ce77c53c9aaebee32c
hagerlabib <88290234+hagerlabib@users.noreply.github.com>
2023-04-09 02:09:20 +0200

    Eloquent test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,11 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::query()
+            ->where('email_verified_at' ,'!=', null)
+            ->orderByDesc('created_at')
+            ->limit(3)
+            ->get(); // replace this with Eloquent statement
 
         return view('users.index', compact('users'));
     }
@@ -23,7 +27,11 @@ class UserController extends Controller
     public function show($userId)
     {
         $user = NULL; // TASK: find user by $userId or show "404 not found" page
-
+        $user = User::where('id',$userId)->first();
+       if (!$user) {
+            abort(404);
+        }
+            
         return view('users.show', compact('user'));
     }
 
@@ -32,6 +40,11 @@ class UserController extends Controller
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
         $user = NULL;
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email
+        ],
+         ['password' => 'password']);
 
         return view('users.show', compact('user'));
     }
@@ -41,6 +54,11 @@ class UserController extends Controller
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
         $user = NULL; // updated or created user
+        $user = User::updateOrCreate([
+            'name' => $name,
+        ],
+         [ 'email' => $email,
+          'password' => 'password']);
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +70,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        User::whereIn('id',$request->users)->delete();
 
         return redirect('/')->with('success', 'Users deleted');
     }
