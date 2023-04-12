7362bba576ceefc20ad0c39468b11922cff8bb0a
doclock4715 <doclock4715@windowslive.com>
2023-04-12 11:08:28 +0300

    Laravel Eloquent

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,6 +26,11 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
+        $old_name = $request->old_name;
+        $new_name = $request->new_name;
+
+        Project::where('name',$old_name)
+        ->update(['name'=> $new_name]);
 
         return redirect('/')->with('success', 'Projects updated');
     }
@@ -35,7 +40,7 @@ class ProjectController extends Controller
         Project::destroy($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects = Project::all();
+        $projects = Project::withTrashed()->get();
 
         return view('projects.index', compact('projects'));
     }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -15,14 +16,22 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')
+            ->orderByDesc('created_at')
+            ->take(3)
+            ->get();
+
 
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        /*  $user = NULL; */// TASK: find user by $userId or show "404 not found" page
+        $user = User::find($userId);
+        if (!$user) {
+            return abort(404);
+        }
 
         return view('users.show', compact('user'));
     }
@@ -31,7 +40,19 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::where('name', $name)
+            ->where('email', $email)
+            ->first();
+
+        if (!$user) {
+            $password = Str::random(8);
+
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => bcrypt($password),
+            ]);
+        }
 
         return view('users.show', compact('user'));
     }
@@ -40,7 +61,18 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::where('name', $name)
+            ->update(['email'=>$email]);
+
+        if (!$user) {
+            $password = Str::random(8);
+
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => bcrypt($password),
+            ]);
+        }
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +84,8 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+
+        User::whereIn('id',$request->users)->delete();
 
         return redirect('/')->with('success', 'Users deleted');
     }
