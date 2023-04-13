bf8d04b4d9d02a461bf355be0fc26fbfa306c742
Muhammad Bintang Ramadhan <107125882+cherryjayhawk@users.noreply.github.com>
2023-04-13 10:19:14 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,14 +15,25 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
-
+//         $users = User::all(); // replace this with Eloquent statement
+        $users = User::where('email_verified_at', !null)
+            ->orderBy('created_at', 'desc')
+            ->take(3)
+            ->get();
+        
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+//         $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::where('id', $userId)->get();
+        
+        if ($user != null) {
+            abort(404);
+        } else {
+            return view('users.show', compact('user'));
+        }
 
         return view('users.show', compact('user'));
     }
@@ -31,17 +42,36 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+//         $user = NULL;
+        $user = User::where([['name', $name],['email', $email]])->get();
 
-        return view('users.show', compact('user'));
+        if ($user == null) {
+            $user = new User;
+            $user->name = $name;
+            $user->email = $email;
+            $user->password = rand();
+
+            return view('users.show', compact('user'));
+        } else {
+            return view('users.show', compact('user'));
+        }
+//         return view('users.show', compact('user'));
     }
 
     public function check_update($name, $email)
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
-
+//         $user = NULL; // updated or created user
+        $user = User::where('name', $name)->get();
+        
+        if ($user != null) {
+            $user->email = $email;
+        } else {
+            $user->name = $name;
+            $user->email = $email;
+            $user->password = rand();
+        }
         return view('users.show', compact('user'));
     }
 
@@ -52,6 +82,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        DB::table('users')->whereIn('id', $request->user)->delete();
 
         return redirect('/')->with('success', 'Users deleted');
     }
