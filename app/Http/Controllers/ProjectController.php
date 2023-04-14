f202c6f71a68ed68aa0076c6a3b23d4e54557919
inam925 <97148236+inam925@users.noreply.github.com>
2023-04-14 11:33:26 +0500

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,10 +26,7 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
-        Project::table('projects')->updateOrCreate(
-            ['name' => $request->old_name],
-            ['name' => $request->new_name]
-        );
+        Project::where('name', $request->old_name)->update(['name' => $request->new_name]);
 
         return redirect('/')->with('success', 'Projects updated');
     }
