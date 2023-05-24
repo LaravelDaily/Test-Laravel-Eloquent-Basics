85b3828c811a59340dbb33b97cb220e1de2aa406
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-05-24 11:36:55 +0300

    Pass all tests successful

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Project;
+use App\Models\Stat;
+
+class ProjectObserver
+{
+    /**
+     * Handle the Project "created" event.
+     */
+    public function created(Project $project): void
+    {
+        Stat::query()->increment('projects_count');
+    }
+}
