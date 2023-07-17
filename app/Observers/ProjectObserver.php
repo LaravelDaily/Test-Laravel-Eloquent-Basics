f0aa15de9efd317eab9c10dccef08a308f870b16
Amine Akacha <akachamohamedamine@gmail.com>
2023-07-17 12:03:34 -0400

    Fix

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Project;
+use App\Models\Stat;
+
+class ProjectObserver
+{
+    /**
+     * Handle the Project "created" event.
+     */
+    public function created(Project $project): void
+    {
+        $stat = Stat::first();
+        $stat->projects_count += 1;
+        $stat->save();
+    }
+
+    /**
+     * Handle the Project "updated" event.
+     */
+    public function updated(Project $project): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "deleted" event.
+     */
+    public function deleted(Project $project): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "restored" event.
+     */
+    public function restored(Project $project): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "force deleted" event.
+     */
+    public function forceDeleted(Project $project): void
+    {
+        //
+    }
+}
