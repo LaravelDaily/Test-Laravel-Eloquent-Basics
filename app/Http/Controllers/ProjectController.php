91ff34a95220c683fe0da53a60b2148f6c8b26fc
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-10 01:05:04 +0330

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,7 +26,7 @@ class ProjectController extends Controller
         //   set name = $request->new_name
         //   where name = $request->old_name
         // Insert Eloquent statement below
-        $project = Project::where('name' , $request->old_name)
+        Project::where('name' , $request->old_name)
         ->update(['name' => $request->new_name]);
 
         return redirect('/')->with('success', 'Projects updated');
