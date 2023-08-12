537a2bb0122e6d1a30bd0139b019ac52ab9c6de0
vpinti <vittorio.pinti@gmail.com>
2023-08-13 00:44:26 +0200

    task completed

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,6 +26,8 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
+        Project::where('name', $request->get('old_name'))
+            ->update(['name' => $request->get('new_name')]);
 
         return redirect('/')->with('success', 'Projects updated');
     }
@@ -35,7 +37,7 @@ class ProjectController extends Controller
         Project::destroy($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects = Project::all();
+        $projects = Project::all()->withTrashed();
 
         return view('projects.index', compact('projects'));
     }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,54 +4,59 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // TASK: turn this SQL query into Eloquent
-        // select * from users
-        //   where email_verified_at is not null
-        //   order by created_at desc
-        //   limit 3
-
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')
+            ->orderByDesc('created_at')
+            ->limit(3)
+            ->get();
 
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::findOrFail($userId); // TASK: find user by $userId or show "404 not found" page
 
         return view('users.show', compact('user'));
     }
 
     public function check_create($name, $email)
     {
-        // TASK: find a user by $name and $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make(Str::password())
+        ]);
 
         return view('users.show', compact('user'));
     }
 
     public function check_update($name, $email)
     {
-        // TASK: find a user by $name and update it with $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        if(User::where('name', $name)->exists()) {
+            $user = User::where('name', $name)->update(['email'=> $email]);
+         }
+         else
+         {
+            $user = User::create([
+                'name' => $name,
+                'email'=>$email,
+                'password' => Hash::make(Str::password())
+            ]);
+         }
 
         return view('users.show', compact('user'));
     }
 
     public function destroy(Request $request)
     {
-        // TASK: delete multiple users by their IDs
-        // SQL: delete from users where id in ($request->users)
-        // $request->users is an array of IDs, ex. [1, 2, 3]
-
-        // Insert Eloquent statement here
+        User::destroy($request->get('users'));
 
         return redirect('/')->with('success', 'Users deleted');
     }
