a2e3a494b6f48ec4c7dbd2c0cf04e6e6ced2cf5a
Usenmfon <usenmfonuko@gmail.com>
2023-09-19 00:57:36 +0100

    All tests passed

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\ActiveScope;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeActive(Builder $query): void
+    {
+        $query->where('email_verified_at', '<>', 'NULL');
+    }
 }
