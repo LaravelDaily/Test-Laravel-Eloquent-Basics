db6f7bf7eaa3cac6e03234e2668848bd9870976a
TheVitik <lokplay2003@gmail.com>
2023-09-19 12:11:10 +0300

    Add task solution

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeActive(Builder $query){
+        $query->whereNotNull('email_verified_at');
+    }
 }
