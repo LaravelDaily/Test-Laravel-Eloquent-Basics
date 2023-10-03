acdd4d994be84573e96c1c02bf1cce6f31bdf726
Hoàng Văn Tuyến <tuyenhv@tienngay.vn>
2023-10-03 22:56:45 +0700

    practice 8/11 task

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,9 +11,7 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         // TASK: Currently this statement fails. Fix the underlying issue.
-        Project::create([
-            'name' => $request->name
-        ]);
+        Project::create(['name' => $request->name]);
 
         return redirect('/')->with('success', 'Project created');
     }
@@ -26,13 +24,16 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
+        Project::query()
+            ->where('name', $request->old_name)
+            ->update(['name' => $request->new_name]);
 
         return redirect('/')->with('success', 'Projects updated');
     }
 
     public function destroy($projectId)
     {
-        Project::destroy($projectId);
+        Project::softDeleted($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
         $projects = Project::all();
