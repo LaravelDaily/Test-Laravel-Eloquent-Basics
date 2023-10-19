4802af32309efa55af81f749adae0fde2e433b77
exe544 <a.lashko97@gmail.com>
2023-10-19 17:27:51 +0300

    done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
     public function show($userId)
     {
         $user = User::findOrFail($userId); // TASK: find user by $userId or show "404 not found" page
-dd($user);
+
         return view('users.show', compact('user'));
     }
 
