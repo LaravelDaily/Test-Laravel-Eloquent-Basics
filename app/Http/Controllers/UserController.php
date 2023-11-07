e5a3dd60e4dc0473df1a749ca8ba0b06fc11f883
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-11-07 08:32:53 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
 
     public function show($userId)
     {
-        $user = $userId; // TASK: find user by $userId or show "404 not found" page
+        $user = User::find($userId); // TASK: find user by $userId or show "404 not found" page
 
         return view('users.show', compact('user'));
     }
