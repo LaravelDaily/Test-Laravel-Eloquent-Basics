3dda97307b27335513816e600bb8b97d54d3aa12
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-11-13 19:39:05 +0800

    Update StatObserver.php

--- app/Observers/StatObserver.php
+++ app/Observers/StatObserver.php
@@ -3,13 +3,15 @@
 namespace App\Observers;
  
 use App\Models\Stat;
+use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
  
-class StatObserver
+class StatObserver implements ShouldHandleEventsAfterCommit
 {
 
     public function updated(Stat $stat): void
     {
         // ...
+        Stat::update(['projects_count'=>$stat->projects_count+1]);
     }
 
 }
