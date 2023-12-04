7c64d0809dae17d2d80bedb8fd08df3e12d71156
John Loyd Lao <67764717+johnloydlao@users.noreply.github.com>
2023-12-04 23:17:39 +0800

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Models\Project;
 
 class EventServiceProvider extends ServiceProvider
 {
