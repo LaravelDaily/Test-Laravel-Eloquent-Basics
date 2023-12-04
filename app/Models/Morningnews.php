9665bf3d4289e1979a8aad3da2f08b0974a9bf09
John Loyd Lao <67764717+johnloydlao@users.noreply.github.com>
2023-12-04 23:31:53 +0800

    Update and rename News.php to Morningnews.php

--- app/Models/Morningnews.php
+++ app/Models/Morningnews.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class News extends Model
+class Morningnews extends Model
 {
     use HasFactory;
 
+    protected $table = 'morning_news';
     protected $fillable = ['title', 'news_text'];
 }
