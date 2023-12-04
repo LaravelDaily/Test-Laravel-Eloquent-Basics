f248c32f0afb39ceaa4dc91b54c605081dfb2a3d
John Loyd Lao <67764717+johnloydlao@users.noreply.github.com>
2023-12-04 23:36:03 +0800

    Update 2021_11_15_061337_create_news_table.php

--- database/migrations/2021_11_15_061337_create_news_table.php
+++ database/migrations/2021_11_15_061337_create_news_table.php
@@ -13,7 +13,7 @@ class CreateNewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('news', function (Blueprint $table) {
+        Schema::create('morning_news', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('news_text');
@@ -28,6 +28,6 @@ class CreateNewsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('news');
+        Schema::dropIfExists('morning_news');
     }
 }
