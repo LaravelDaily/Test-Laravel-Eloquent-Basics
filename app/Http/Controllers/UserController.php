3ef422cd38038ff578f93becca70569c5ea739e0
John Loyd Lao <67764717+johnloydlao@users.noreply.github.com>
2023-12-04 21:59:00 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,7 +64,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
-         User::where('id', collect($request->users))->delete();
+        User::whereIn('id', $request->users)->delete();
 
         return redirect('/')->with('success', 'Users deleted');
     }
