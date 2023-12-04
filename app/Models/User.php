39b04ec7b3daaa80f3ce768f8238d8607d27ce2e
John Loyd Lao <67764717+johnloydlao@users.noreply.github.com>
2023-12-04 22:37:10 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Builder;
 
 class User extends Authenticatable
 {
