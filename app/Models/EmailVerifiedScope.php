703b0bc21063b3a4d4b19c095579b7d83e877c01
John Loyd Lao <67764717+johnloydlao@users.noreply.github.com>
2023-12-04 22:28:24 +0800

    Update EmailVerifiedScope.php

--- app/Models/EmailVerifiedScope.php
+++ app/Models/EmailVerifiedScope.php
@@ -1,18 +1,1 @@
-<?php
- 
-namespace App\Models\Scopes;
- 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Scope;
- 
-class EmailVerifiedScope implements Scope
-{
-    /**
-     * Apply the scope to a given Eloquent query builder.
-     */
-    public function scopeActive(Builder $builder): void
-    {
-        $builder->whereNotNull('email_verified_at');
-    }
-}
+
