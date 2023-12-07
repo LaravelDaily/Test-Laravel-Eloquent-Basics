3ca04a1a09442e2f0425650e84fca1d49d9362e2
bilawal shaikh <46257281+bilawalshaikh@users.noreply.github.com>
2023-12-07 17:42:49 +0500

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
-use App\Models\Stat;
+use App\Models\Stat;h
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -26,6 +26,8 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
+ Project::where('name', $request->old_name)
+        ->update(['name' => $request->new_name]);
 
         return redirect('/')->with('success', 'Projects updated');
     }
