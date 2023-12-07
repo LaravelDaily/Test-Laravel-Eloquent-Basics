771ab6da921aa3c6b6ce49ae1b0170be50fcb3a7
bilawal shaikh <46257281+bilawalshaikh@users.noreply.github.com>
2023-12-07 18:15:15 +0500

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
-use App\Models\Stat;h
+use App\Models\Stat;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -26,7 +26,8 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
- Project::where('name', $request->old_name)
+
+         Project::where('name', $request->old_name)
         ->update(['name' => $request->new_name]);
 
         return redirect('/')->with('success', 'Projects updated');
@@ -37,7 +38,7 @@ class ProjectController extends Controller
         Project::destroy($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects = Project::all();
+        $projects = Project::withTrashed()->get();
 
         return view('projects.index', compact('projects'));
     }
