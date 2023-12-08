6ad9dbd02057bd0604a19542705a5aee308aa675
bilawal shaikh <46257281+bilawalshaikh@users.noreply.github.com>
2023-12-08 10:08:21 +0500

    Update ProjectObserver.php

--- app/Observers /ProjectObserver.php
+++ app/Observers /ProjectObserver.php
@@ -1,1 +1,49 @@
+<?php
 
+namespace App\Observers;
+
+use App\Models\Stat;
+use App\Models\Project;
+
+class ProjectObserver
+{
+    /**
+     * Handle the Project "created" event.
+     */
+    public function created(Project $project): void
+    {
+        Stat::increment('projects_count', 1);
+    }
+
+    /**
+     * Handle the Project "updated" event.
+     */
+    public function updated(Project $project): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "deleted" event.
+     */
+    public function deleted(Project $project): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "restored" event.
+     */
+    public function restored(Project $project): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Project "force deleted" event.
+     */
+    public function forceDeleted(Project $project): void
+    {
+        //
+    }
+}
