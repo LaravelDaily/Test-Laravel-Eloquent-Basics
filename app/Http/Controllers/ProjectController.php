5047796a6f220dc8e1d81a93824215b685f56581
bilawal shaikh <46257281+bilawalshaikh@users.noreply.github.com>
2023-12-08 10:59:45 +0500

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,7 +26,7 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
-
+        Project::where('name', $request->old_name)->update(['name' => $request->new_name]);
         return redirect('/')->with('success', 'Projects updated');
     }
 
