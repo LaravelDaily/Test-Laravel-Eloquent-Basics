89e4c210859360b76baa00b0c25c8fb0a10bed6a
Azad Kamarbandi <akamarbandi@unitedprint.rdb>
2023-12-10 23:46:55 +0100

    completed

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
@@ -41,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeActive(Builder $query): void
+    {
+        $query->whereNotNull('email_verified_at');
+    }
 }
