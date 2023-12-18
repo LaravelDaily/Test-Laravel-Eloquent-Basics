14d7b7f1fe5b564e31f021a21f32b52174b430e7
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-12-18 17:04:17 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Hash;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
