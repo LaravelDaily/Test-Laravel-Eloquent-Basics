a3b3f4afd6d4fb774e8506ecee3837d8a1e37eea
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-12-18 18:50:41 +0800

    Update ProjectObserver.php

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -10,7 +10,7 @@ class ProjectObserver
     /**
      * Handle the User "created" event.
      */
-    public function updateed(Project $project): void
+    public function updated(Project $project): void
     {
         // ...
       Stat::increment('projects_count',1);
