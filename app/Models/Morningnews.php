357fee8ed3e61fc52f1c5365aad50a154dab6f58
Sudeep-K <pong32btl@gmail.com>
2024-01-24 11:18:59 +0545

    check to see if test passes

--- app/Models/Morningnews.php
+++ app/Models/Morningnews.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Morningnews extends Model
+class MorningNews extends Model
 {
     use HasFactory;
+
+    
 
     protected $fillable = ['title', 'news_text'];
 }
