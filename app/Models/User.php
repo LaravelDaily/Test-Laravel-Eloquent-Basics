b1b5081e3856b28e1b15736f4708f4583d03b9c6
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-01-27 23:57:25 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Illuminate\Database\Eloquent\Builder;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -41,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function scopeActive(Builder $query){
+        $query->whereNotNull('email_verified_at');
+    }
 }
