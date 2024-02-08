15f3ea6b75a6463b17a035442a7b4ffe51ed8891
Ahmad Essam <72272968+ahmadessam5@users.noreply.github.com>
2024-02-08 16:32:11 +0200

    Task 6

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,6 +26,8 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
+        Project::where('name' , $request->old_name)
+                ->update('name' , $request->new_name);
 
         return redirect('/')->with('success', 'Projects updated');
     }
