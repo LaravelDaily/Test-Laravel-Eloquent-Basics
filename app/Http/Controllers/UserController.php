7118f01745a92ace9fae8c42481713b8c1a25ee4
Ahmad Essam <72272968+ahmadessam5@users.noreply.github.com>
2024-02-08 16:14:24 +0200

    Task 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,8 +26,10 @@ class UserController extends Controller
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        //$user = NULL; // TASK: find user by $userId or show "404 not found" page
 
+        $user = User::FindOrFail($userId);
+        
         return view('users.show', compact('user'));
     }
 
