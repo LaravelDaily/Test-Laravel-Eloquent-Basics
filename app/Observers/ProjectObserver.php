942b103a94a184cb8883673b7851918080b17218
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-02-14 01:52:42 +0800

    Create ProjectObserver.php

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,20 @@
+<?php
+ 
+namespace App\Observers;
+ 
+use App\Models\Project;
+use App\Models\Stat;
+ 
+class ProjectObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(Project $project): void
+    {
+        // ...
+      Stat::increment('projects_count');
+    }
+ 
+    
+}
