9b05f9c398e90e5a585290b63720d7ce060da8c2
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-02-14 00:48:54 +0800

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,7 +46,12 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::updateOrCreate([
+                                     'name'=>$name
+        ],[
+                                     'email'=>$email
+                                     'password'=>Hash::make('random_password')
+                                     ]); // updated or created user
 
         return view('users.show', compact('user'));
     }
@@ -58,6 +63,8 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        User::destroy($request->users);
+        
 
         return redirect('/')->with('success', 'Users deleted');
     }
