c9b1570140482e20701b88424ff3a1ce687edafb
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-02-20 19:40:02 +0800

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,9 +11,13 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         // TASK: Currently this statement fails. Fix the underlying issue.
-        Project::create([
-            'name' => $request->name
-        ]);
+        // Project::create([
+        //     'name' => $request->name
+        // ]);
+
+        $project=new Project;
+        $project->name=$request->name;
+        $project->save();
 
         return redirect('/')->with('success', 'Project created');
     }
@@ -26,6 +30,7 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
+        Project::where('name',$request->old_name)->update(['name'=>$request->new_name]);
 
         return redirect('/')->with('success', 'Projects updated');
     }
