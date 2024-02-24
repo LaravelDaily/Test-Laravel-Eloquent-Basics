fc2dc214d36a187d0dd0aa36d8ec98a14b6b50d9
Xuan <108394451+xuanxuan27@users.noreply.github.com>
2024-02-24 20:39:10 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function scopeActive(Builder $query)
+    public function scopeActive($query)
     {
         return $query->whereNotNull('email_verified_at');
     }
