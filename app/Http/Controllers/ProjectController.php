9e203bbbacb3a947c84e2e5b84cfef6067f275e7
Eden <tranngocquang13199@gmail.com>
2024-03-18 00:31:40 +0700

    task:comple all

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,9 +11,12 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         // TASK: Currently this statement fails. Fix the underlying issue.
-        Project::create([
+        $project = Project::create([
             'name' => $request->name
         ]);
+        // $project = new Project();
+        // $project->name = $request->name;
+        // $project->save();
 
         return redirect('/')->with('success', 'Project created');
     }
@@ -26,7 +29,8 @@ class ProjectController extends Controller
         //   where name = $request->old_name
 
         // Insert Eloquent statement below
-
+        Project::where('name', $request->old_name)
+            ->update(['name' => $request->new_name]);
         return redirect('/')->with('success', 'Projects updated');
     }
 
@@ -35,7 +39,7 @@ class ProjectController extends Controller
         Project::destroy($projectId);
 
         // TASK: change this Eloquent statement to include the soft-deletes records
-        $projects = Project::all();
+        $projects = Project::withTrashed()->get();
 
         return view('projects.index', compact('projects'));
     }
@@ -50,5 +54,4 @@ class ProjectController extends Controller
 
         return redirect('/')->with('success', 'Project created');
     }
-
 }
